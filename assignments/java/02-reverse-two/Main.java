63032ca8e9c6a0dbf7066a21c805f0a9bc3e5424
wickedMutantTurtle <shantel.omondi2010@gmail.com>
2026-09-15 21:32:14 +0000

    Submit: java/02-reverse-two

--- assignments/java/02-reverse-two/Main.java
+++ assignments/java/02-reverse-two/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your solution here.
+        int num1=scanner.nextInt();
+        int num2=scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println(num2 +" "+num1);
     }
 }
