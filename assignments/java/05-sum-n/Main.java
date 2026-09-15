e6325bfc4b3fd4a3595c8fe2726e67c804665119
wickedMutantTurtle <shantel.omondi2010@gmail.com>
2026-09-15 22:02:53 +0000

    Submit: java/05-sum-n

--- assignments/java/05-sum-n/Main.java
+++ assignments/java/05-sum-n/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your solution here.
+        int amt= scanner.nextInt();
+        int[] nums= new int[amt];
+
+        for(int i=0; i<amt; i++) nums[i]=scanner.nextInt();
+        
+        int sum=0;
+        for(int current:nums) sum+=current;
+
+        System.out.println(sum); 
     }
 }
