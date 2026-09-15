2518d61e0f1a2d80e0888e7e5631f7b35a12f44c
wickedMutantTurtle <shantel.omondi2010@gmail.com>
2026-09-15 21:57:06 +0000

    Submit: java/04-sum-two

--- assignments/java/04-sum-two/Main.java
+++ assignments/java/04-sum-two/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your solution here.
+        int[] nums= new int[2];
+
+        for(int i=0; i<2; i++) nums[i]= scanner.nextInt();
+
+        System.out.println(nums[0]+nums[1]);
     }
 }
