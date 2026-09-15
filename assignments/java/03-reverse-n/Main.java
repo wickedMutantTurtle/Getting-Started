5c64ba3029e41a4f89370ff11b3cc692011b57c7
wickedMutantTurtle <shantel.omondi2010@gmail.com>
2026-09-15 21:52:48 +0000

    Submit: java/03-reverse-n

--- assignments/java/03-reverse-n/Main.java
+++ assignments/java/03-reverse-n/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your solution here.
+        int amt= scanner.nextInt();
+        int[] uwu = new int[amt];
+        for (int i=0; i<amt;i++) uwu[i] = scanner.nextInt();
+        for (int i = amt-1; i >= 0; i--) System.out.print(uwu[i] + " ");
     }
 }
