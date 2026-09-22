85d25595b3a5b3668f804b4a0c1183a92b534220
wickedMutantTurtle <shantel.omondi2010@gmail.com>
2026-09-22 21:22:58 +0000

    Submit: java/06-modulo-two

--- assignments/java/06-modulo-two/Main.java
+++ assignments/java/06-modulo-two/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your solution here.
+        int[] num = new int[2];
+        for(int i=0; i<num.length; i++) num[i]=scanner.nextInt();
+
+        
+        IO.println(num[0]%num[1]);
     }
 }
